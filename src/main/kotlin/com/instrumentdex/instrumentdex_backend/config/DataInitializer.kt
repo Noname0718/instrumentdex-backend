55859eee2b2dee540a6d242ccb08e9780f6af006
noname0718 <thckdals3@gmail.com>
2025-11-24 12:22:04 +0900

    곡 API 연동 및 연습곡 UI 개편

--- src/main/kotlin/com/instrumentdex/instrumentdex_backend/config/DataInitializer.kt
+++ src/main/kotlin/com/instrumentdex/instrumentdex_backend/config/DataInitializer.kt
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.instrumentdex.instrumentdex_backend.domain.instrument.Instrument
 import com.instrumentdex.instrumentdex_backend.domain.song.PracticeSong
+import com.instrumentdex.instrumentdex_backend.domain.song.Song
 import com.instrumentdex.instrumentdex_backend.repository.InstrumentRepository
 import com.instrumentdex.instrumentdex_backend.repository.PracticeSongRepository
+import com.instrumentdex.instrumentdex_backend.repository.SongRepository
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.core.io.ClassPathResource
@@ -15,7 +17,8 @@ import org.springframework.stereotype.Component
 @Component
 class DataInitializer(
     private val instrumentRepository: InstrumentRepository,
-    private val practiceSongRepository: PracticeSongRepository
+    private val practiceSongRepository: PracticeSongRepository,
+    private val songRepository: SongRepository
 ) : CommandLineRunner{
     private val log = LoggerFactory.getLogger(javaClass)
     private val objectMapper = jacksonObjectMapper()
@@ -36,7 +39,8 @@ class DataInitializer(
             val root: JsonNode = objectMapper.readTree(input)
 
             val instrumentsNode = root["instruments"]
-            val songsNode = root["practice_songs"]
+            val practiceSongsNode = root["practice_songs"]
+            val songsNode = root["songs"]
 
             if (instrumentsNode != null && instrumentsNode.isArray) {
                 val instruments: List<Instrument> =
@@ -45,11 +49,18 @@ class DataInitializer(
                 log.info("Inserted ${instruments.size} instruments.")
             }
 
-            if (songsNode != null && songsNode.isArray) {
+            if (practiceSongsNode != null && practiceSongsNode.isArray) {
                 val songs: List<PracticeSong> =
-                    objectMapper.readValue(songsNode.toString())
+                    objectMapper.readValue(practiceSongsNode.toString())
                 practiceSongRepository.saveAll(songs)
                 log.info("Inserted ${songs.size} practice songs.")
+            }
+
+            if (songsNode != null && songsNode.isArray) {
+                val songs: List<Song> =
+                    objectMapper.readValue(songsNode.toString())
+                songRepository.saveAll(songs)
+                log.info("Inserted ${songs.size} songs.")
             }
         }
     }
